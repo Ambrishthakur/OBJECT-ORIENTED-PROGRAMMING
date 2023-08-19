cd819040d3ce066de51e20f54673840c24832d12
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2023-08-20 00:25:38 +0530

    Add files via upload

---
+++ operators2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    bool a = true;
+    bool b = false;
+
+    cout<<(a&&b)<<endl;          //(Logical operators)
+    cout<<(a||b)<<endl;
+    cout<<(!a==b)<<endl;
+
+    return 0;
+
+}
