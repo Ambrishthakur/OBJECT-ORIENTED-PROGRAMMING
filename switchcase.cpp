59e3dc42d9742c0363c907a307a5049f8cc67809
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2023-09-06 22:42:03 +0530

    Add files via upload

---
+++ switchcase.cpp
@@ -0,0 +1,85 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+
+    switch(day);
+    cin>>day;
+
+    case1:
+    cout<<"monday"<<endl;
+    break;
+
+    case2:
+    cout<<"tuesday"<<endl;
+    break;
+
+    case3:
+    cout<<"wednesday"<<endl;
+    break;
+
+    case4:
+    cout<<"thrusday"<<endl;
+    break;
+
+    case5:
+    cout<,"friday"<<endl;
+    break;
+
+    case6:
+    cout<<"saturday"<<endl;
+    break;
+
+    case7:
+    cout<<"sunday"<<endl;
+    break;
+
+}*/
+
+ #include <iostream>
+ using namespace std;
+
+
+//  int main(){
+
+//     char(character);
+//     cin>>character;
+
+//     switch(character)
+//     {
+
+//     case 'a':
+//     cout<<"vowel"<<endl;
+//     break;
+
+//     case 'e':
+//     cout<<"vowel"<<endl;
+//     break;
+
+//     case 'i':
+//     cout<<"vowel"<<endl;
+//     break;
+
+//     case 'o':
+//     cout<<"vowel"<<endl;
+//     break;
+
+//     case 'u':
+//     cout<<"vowel"<<endl;
+//     break;
+
+//     default:
+//     cout<<"consonants"<<endl;
+//     break;
+//     }
+
+
+
+
+// }
+int main() {
+    std :: cout << "hello world";
+    return 0;
+
+}
+
